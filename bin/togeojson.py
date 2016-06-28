a9e92958a4638c998045f7ba22d64da61d93f099
David Jones <drj@pobox.com>
2016-06-28 21:38:39 +0100

    Remove unflighted airports

--- bin/togeojson.py
+++ bin/togeojson.py
@@ -9,12 +9,26 @@ def main():
     geo = dict(type='FeatureCollection',
       features=features)
     with open('airports.ussv') as u:
-        for row in csv.reader(u, delimiter='\037'):
-            print(row)
-            lat, lon = map(float, row[6:8])
-            features.append(dict(type='Feature',
-              geometry=dict(type='Point',
-                coordinates=[lon, lat])))
+        airports = [row for row in csv.reader(u, delimiter='\037')]
+
+    with open('routes.dat') as r:
+        routes = [row for row in csv.reader(r)]
+
+    # All airports that appear in some route
+    sources = set(route[2] for route in routes)
+    destinations = set(route[4] for route in routes)
+    routed = sources | destinations
+
+    print(len(airports))
+    airports = [airport for airport in airports
+      if airport[4] in routed or airport[5] in routed]
+    print(len(airports))
+
+    for row in airports:
+        lat, lon = map(float, row[6:8])
+        features.append(dict(type='Feature',
+          geometry=dict(type='Point',
+            coordinates=[lon, lat])))
     with open('airports.geojson', 'wb') as j:
         json.dump(geo, j)
             
