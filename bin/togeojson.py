e528d53431309f50049804f82df2afef7fc0e7eb
David Jones <drj@pobox.com>
2016-06-29 22:53:31 +0100

    Use Airport namedtuple

--- bin/togeojson.py
+++ bin/togeojson.py
@@ -4,6 +4,11 @@ import csv
 import json
 import sys
 
+from collections import namedtuple
+
+Airport = namedtuple("Airport",
+  "index name city country code icao latitude longitude altitude timezone dst tz")
+
 def main():
     features = []
     geo = dict(type='FeatureCollection',
@@ -14,21 +19,32 @@ def main():
     with open('routes.dat') as r:
         routes = [row for row in csv.reader(r)]
 
-    # All airports that appear in some route
+    # All airport codes that appear in some route
     sources = set(route[2] for route in routes)
     destinations = set(route[4] for route in routes)
     routed = sources | destinations
 
+    # Filter airports to only those that have routes
     print(len(airports))
     airports = [airport for airport in airports
-      if airport[4] in routed or airport[5] in routed]
+      if airport[4] in routed]
     print(len(airports))
 
+    # Convert lat/lon to numeric.
     for row in airports:
-        lat, lon = map(float, row[6:8])
+        row[6] = float(row[6])
+        row[7] = float(row[7])
+
+    # Convert to namedtuple
+    airports = [Airport(*row) for row in airports]
+
+    airport_dict = {airport.code: airport for airport in airports}
+
+
+    for row in airports:
         features.append(dict(type='Feature',
           geometry=dict(type='Point',
-            coordinates=[lon, lat])))
+            coordinates=[row.longitude, row.latitude])))
     with open('airports.geojson', 'wb') as j:
         json.dump(geo, j)
             
